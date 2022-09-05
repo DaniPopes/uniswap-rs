c3fcef92b18d8d3cddc1be4c39717cfcbc5a9582
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2022-09-05 01:09:52 +0000

    feat: improve pair

--- src/pair.rs
+++ src/pair.rs
@@ -2,13 +2,54 @@ use crate::{
     bindings::i_uniswap_v2_pair::IUniswapV2Pair, factory::Factory, UniswapV2Library,
     UniswapV2LibraryError,
 };
-use ethers::prelude::{builders::ContractCall, *};
+use ethers::{
+    abi::Token,
+    core::abi::Detokenize,
+    prelude::{builders::ContractCall, *},
+};
 use std::sync::Arc;
 
 type Tokens = (Address, Address);
 type Reserves = (u128, u128, u32);
 
-/// Represents a UniswapV2 liquidity pair, composed of 2 different tokens.
+#[derive(Debug, thiserror::Error)]
+pub enum PairError<M: Middleware> {
+    #[error(transparent)]
+    ContractError(#[from] ContractError<M>),
+    #[error(transparent)]
+    UniswapV2LibraryError(#[from] UniswapV2LibraryError),
+    #[error(transparent)]
+    MulticallError(#[from] MulticallError<M>),
+}
+
+type Result<T, M> = std::result::Result<T, PairError<M>>;
+
+/// Represents a UniswapV2 liquidity pair, composed of 2 different ERC20 tokens.
+///
+/// # Example
+///
+/// ```no_run
+/// use ethers::prelude::*;
+/// use uniswap_rs::Pair;
+/// use std::{convert::TryFrom, sync::Arc};
+///
+/// # async fn foo() -> Result<(), Box<dyn std::error::Error>> {
+/// // Example UniswapV2 liquidity pair on mainnet
+/// let weth_usdc_address: Address = "0xB4e16d0168e52d35CaCD2c6185b44281Ec28C9Dc".parse().unwrap();
+/// let client = Provider::<Http>::try_from("http://localhost:8545").unwrap();
+/// let client = Arc::new(client);
+/// let mut pair = Pair::new(client, weth_usdc_address);
+///
+/// pair.sync(true).await?;
+///
+/// let reserves = pair.reserves().unwrap();
+/// assert!(reserves.0 > 0);
+/// assert!(reserves.1 > 0);
+/// assert!(reserves.2 > 0);
+///
+/// assert!(pair.deployed());
+/// # Ok(())
+/// # }
 #[derive(Clone)]
 pub struct Pair<M> {
     /// The client.
@@ -17,14 +58,14 @@ pub struct Pair<M> {
     /// The pair address. Might not be currently deployed.
     address: Address,
 
-    /// The tokens of the pair.
+    /// The ordered tokens of the pair.
     tokens: Option<Tokens>,
 
     /// Whether the pair is currently deployed in the client's network.
     deployed: bool,
 
     /// The token reserves of the pair.
-    reserves: Reserves,
+    reserves: Option<Reserves>,
 }
 
 // Skip client in formatting
@@ -39,64 +80,284 @@ impl<M> std::fmt::Debug for Pair<M> {
     }
 }
 
+impl<M: Middleware> From<Pair<M>> for IUniswapV2Pair<M> {
+    fn from(pair: Pair<M>) -> Self {
+        Self::new(pair.address, pair.client)
+    }
+}
+
+impl<M: Middleware> From<&Pair<M>> for IUniswapV2Pair<M> {
+    fn from(pair: &Pair<M>) -> Self {
+        Self::new(pair.address, pair.client.clone())
+    }
+}
+
+impl<M: Middleware> From<&mut Pair<M>> for IUniswapV2Pair<M> {
+    fn from(pair: &mut Pair<M>) -> Self {
+        Self::new(pair.address, pair.client.clone())
+    }
+}
+
 impl<M: Middleware> Pair<M> {
-    pub async fn new(
+    pub fn new(client: Arc<M>, address: Address) -> Self {
+        Self { client, address, tokens: None, deployed: false, reserves: None }
+    }
+
+    pub fn new_with_tokens(
         client: Arc<M>,
         factory: Factory,
-        tokens: Option<Tokens>,
-        address: Option<Address>,
-    ) -> Result<Self, UniswapV2LibraryError> {
-        let address = if let Some(address) = address {
-            address
-        } else {
-            let tokens =
-                tokens.expect("Must provide either a pair address or the two token addresses");
-            UniswapV2Library::pair_for(factory, tokens.0, tokens.1)?
-        };
+        token0: Address,
+        token1: Address,
+    ) -> Result<Self, M> {
+        let (token0, token1) = UniswapV2Library::sort_tokens(token0, token1)?;
+        let address = UniswapV2Library::pair_for(factory, token0, token1)?;
 
-        Ok(Self { client, address, tokens, deployed: false, reserves: (0, 0, 0) })
+        Ok(Self {
+            client,
+            address,
+            tokens: Some((token0, token1)),
+            deployed: false,
+            reserves: None,
+        })
     }
 
-    /// Manually set whether the pair has been deployed.
-    pub fn deployed(&mut self, deployed: bool) -> &mut Self {
-        self.deployed = deployed;
-        self
+    /// Returns the address of the pair.
+    pub fn address(&self) -> Address {
+        self.address
     }
 
-    /// Manually set the reserves of the pair.
-    pub fn reserves(&mut self, reserves: Reserves) -> &mut Self {
-        self.reserves = reserves;
-        self
+    /// Returns whether the pair has been deployed. If it hasn't been synced yet, this will be equal
+    /// to false.
+    pub fn deployed(&self) -> bool {
+        self.deployed
+    }
+
+    /// Returns the reserves of the pair. If it hasn't been synced yet, this will be equal to (0, 0,
+    /// 0).
+    pub fn reserves(&self) -> Option<Reserves> {
+        self.reserves
     }
 
     /// Returns the contract calls for getting the addresses of the pair's tokens.
     pub fn get_tokens(&self) -> (ContractCall<M, Address>, ContractCall<M, Address>) {
-        let pair = IUniswapV2Pair::new(self.address, self.client.clone());
+        let pair = IUniswapV2Pair::from(self);
         (pair.token_0(), pair.token_1())
     }
 
     /// Returns the contract call for getting the reserves of the pair.
     pub fn get_reserves(&self) -> ContractCall<M, Reserves> {
-        IUniswapV2Pair::new(self.address, self.client.clone()).get_reserves()
+        IUniswapV2Pair::from(self).get_reserves()
     }
 
-    /// Set the reserves and deployed status of the pair by querying the blockchain.
-    pub async fn set_reserves(&mut self) -> Result<&mut Self, ContractError<M>> {
-        let call = self.get_reserves();
-        let (deployed, reserves) = match call.call().await {
-            Ok(reserves) => (true, reserves),
-            Err(e) => {
-                match e {
-                    // Contract has not yet been deployed
-                    ContractError::ContractNotDeployed => (false, (0, 0, 0)),
-                    e => return Err(e),
-                }
-            }
-        };
+    /// Syncs the tokens and reserves of the pair by querying the blockchain.
+    ///
+    /// Assumes that any call failure means the pair has not been deployed yet.
+    pub async fn sync(&mut self, sync_reserves: bool) -> Result<&mut Self, M> {
+        let sync_reserves = sync_reserves || self.reserves.is_none();
+        let sync_tokens = self.tokens.is_none() || !self.deployed;
 
-        self.deployed = deployed;
-        self.reserves = reserves;
+        let multicall = Multicall::new(self.client.clone(), None).await?;
+        let mut multicall = multicall.version(MulticallVersion::Multicall3);
+
+        if sync_tokens {
+            let calls = self.get_tokens();
+            multicall.add_call(calls.0, true);
+            multicall.add_call(calls.1, true);
+        }
+
+        if sync_reserves {
+            multicall.add_call(self.get_reserves(), true);
+        }
+
+        let result = multicall.call_raw().await?;
+
+        // Assume any call failure means the contract has not been deployed yet
+        match (sync_tokens, sync_reserves) {
+            (true, true) => {
+                let tokens = parse_tokens_result(result[0..2].to_vec())?;
+                let reserves = parse_reserves_result(result[2..].to_vec())?;
+
+                if tokens.is_none() || reserves.is_none() {
+                    self.tokens = None;
+                    self.deployed = false;
+                    return Ok(self)
+                }
+
+                self.deployed = true;
+                self.tokens = tokens;
+                self.reserves = reserves;
+            }
+            (true, false) => {
+                let tokens = parse_tokens_result(result)?;
+
+                if tokens.is_none() {
+                    self.tokens = None;
+                    self.deployed = false;
+                    return Ok(self)
+                }
+
+                self.deployed = true;
+                self.tokens = tokens;
+            }
+            (false, true) => {
+                let reserves = parse_reserves_result(result)?;
+
+                if reserves.is_none() {
+                    self.tokens = None;
+                    self.deployed = false;
+                    return Ok(self)
+                }
+
+                self.deployed = true;
+                self.reserves = reserves;
+            }
+            (false, false) => {}
+        }
 
         Ok(self)
+    }
+}
+
+/// Parses (bool, String) from a vector of tokens.
+fn parse_errors(tokens: Vec<Token>) -> Vec<Option<String>> {
+    type ErrorResult = (bool, String);
+    let mut msgs = vec![];
+
+    for token in tokens {
+        match ErrorResult::from_tokens(vec![token]) {
+            Ok((_, msg)) => msgs.push(Some(msg)),
+            Err(_) => msgs.push(None),
+        }
+    }
+
+    msgs
+}
+
+/// Parses a multicall result from a vector of tokens, returning None if the call returned an error.
+fn parse_result<M: Middleware, D: Detokenize>(tokens: Vec<Token>) -> Result<Option<D>, M> {
+    let res = D::from_tokens(tokens.clone());
+    match res {
+        Err(e) => {
+            // Failed to decode
+            let errors = parse_errors(tokens);
+            if errors.iter().any(|s| s.is_none()) {
+                // Failed to decode errors too
+                Err(ContractError::DetokenizationError(e).into())
+            } else {
+                // All calls failed while allowed
+                Ok(None)
+            }
+        }
+        Ok(res) => Ok(Some(res)),
+    }
+}
+
+/// Parses a multicall result of Pair::get_tokens(), returning None if the call returned an error.
+fn parse_tokens_result<M: Middleware>(tokens: Vec<Token>) -> Result<Option<Tokens>, M> {
+    type TokensResult = ((bool, Address), (bool, Address));
+    let res: Option<TokensResult> = parse_result(tokens)?;
+
+    match res {
+        Some(res) => {
+            if res.0 .0 && res.1 .0 {
+                Ok(Some((res.0 .1, res.1 .1)))
+            } else {
+                Ok(None)
+            }
+        }
+        None => Ok(None),
+    }
+}
+
+/// Parses a multicall result of Pair::get_reserves(), returning None if the call returned an error.
+fn parse_reserves_result<M: Middleware>(tokens: Vec<Token>) -> Result<Option<Reserves>, M> {
+    type ReservesResult = (bool, Reserves);
+    let res: Option<ReservesResult> = parse_result(tokens)?;
+
+    match res {
+        Some(res) => {
+            if res.0 {
+                Ok(Some(res.1))
+            } else {
+                Ok(None)
+            }
+        }
+        None => Ok(None),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    #[allow(unused)]
+    fn test_parsing() {
+        let addresses = (Address::random(), Address::random());
+        let tokens = vec![Token::Address(addresses.0), Token::Address(addresses.1)];
+        let reserve_uints = (69u128, 420u128, 1337u32);
+        let reserves = vec![
+            Token::Uint(reserve_uints.0.into()),
+            Token::Uint(reserve_uints.1.into()),
+            Token::Uint(reserve_uints.2.into()),
+        ];
+        let error_message = "Error message".to_string();
+
+        type SuccessResult = ((bool, Address), (bool, Address), (bool, Reserves));
+        let success_result: SuccessResult =
+            ((true, addresses.0), (true, addresses.1), (true, reserve_uints));
+        let success_tokens = vec![
+            Token::Tuple(vec![Token::Bool(true), Token::Address(addresses.0)]),
+            Token::Tuple(vec![Token::Bool(true), Token::Address(addresses.1)]),
+            Token::Tuple(vec![Token::Bool(true), Token::Tuple(reserves)]),
+        ];
+        type FailureResult = ((bool, String), (bool, String), (bool, String));
+        let failure_result: FailureResult = (
+            (true, error_message.clone()),
+            (true, error_message.clone()),
+            (true, error_message.clone()),
+        );
+        let failure_tokens = vec![
+            Token::Tuple(vec![Token::Bool(false), Token::String(error_message.clone())]),
+            Token::Tuple(vec![Token::Bool(false), Token::String(error_message.clone())]),
+        ];
+
+        // parse_errors
+
+        let errors = parse_errors(success_tokens);
+        for e in errors {
+            assert!(e.is_none());
+        }
+
+        let errors = parse_errors(failure_tokens);
+        for e in errors {
+            assert_eq!(e.unwrap(), error_message.clone());
+        }
+
+        // TODO: cannot infer M
+
+        // // parse_result
+
+        // let result: Option<SuccessResult> = parse_result(tokens).unwrap();
+        // assert_eq!(result.unwrap(), success_result);
+
+        // let result: Option<FailureResult> = parse_result(tokens).unwrap();
+        // assert_eq!(result.unwrap(), failure_result);
+
+        // // parse_tokens_result
+
+        // let result = parse_tokens_result(success_tokens[0..2].to_vec()).unwrap().unwrap();
+        // assert_eq!(result, addresses);
+
+        // let result = parse_tokens_result(failure_tokens).unwrap();
+        // assert!(result.is_none());
+
+        // // parse_reserves_result
+
+        // let result = parse_reserves_result(success_tokens[2..].to_vec()).unwrap().unwrap();
+        // assert_eq!(result, reserve_uints);
+
+        // let result = parse_reserves_result(failure_tokens).unwrap();
+        // assert!(result.is_none());
     }
 }
