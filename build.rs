d8c102bfd03204def8f1189e83735842957515ad
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2022-12-08 09:53:02 +0100

    chore: add check option to build script

--- build.rs
+++ build.rs
@@ -2,16 +2,29 @@ use ethers_contract_abigen::MultiAbigen;
 
 const ABI_PATH: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/abi");
 const BINDINGS_PATH: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/src/contracts/bindings");
+const ABIGEN_CHECK: &str = "ABIGEN_CHECK";
 
 fn main() {
-    // ignore fails, eg for docs.rs builds
-    abigen().unwrap_or_else(|e| eprintln!("Failed to abigen: {e}"));
+    match std::env::var(ABIGEN_CHECK) {
+        Ok(x) if !x.is_empty() => {
+            abigen(true).unwrap();
+        }
+        _ => {
+            // ignore fails, eg for docs.rs builds
+            abigen(false).unwrap_or_else(|e| eprintln!("Failed to abigen: {e}"));
+        }
+    }
 }
 
-fn abigen() -> eyre::Result<()> {
+fn abigen(check: bool) -> eyre::Result<()> {
     let gen = MultiAbigen::from_json_files(ABI_PATH)?;
     let bindings = gen.build()?;
-    bindings.write_to_module(BINDINGS_PATH, false)?;
+    if check {
+        bindings.ensure_consistent_module(BINDINGS_PATH, false)?;
+    } else {
+        bindings.write_to_module(BINDINGS_PATH, false)?;
+    }
     println!("cargo:rerun-if-changed={ABI_PATH}");
+    println!("cargo:rerun-if-env-changed={ABIGEN_CHECK}");
     Ok(())
 }
