b455db70decdfa7f81c8960c9f01529de235213d
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2023-01-06 14:06:47 +0100

    docs: override auto-generated bindings docs

--- src/contracts/mod.rs
+++ src/contracts/mod.rs
@@ -3,5 +3,15 @@
 #[cfg(feature = "addresses")]
 pub mod addresses;
 
+// override docs
+/// Type-safe bindings to the Uniswap smart contracts.
+///
+/// Automatically generated using [`Abigen`][ethers::contract::Abigen].
 #[allow(missing_docs)]
-pub mod bindings;
+pub mod bindings {
+    #[path = "../bindings/mod.rs"]
+    #[doc(hidden)]
+    mod _bindings;
+
+    pub use _bindings::*;
+}
