157a1fcc9b4d42533fd7f96805392becf06f96bd
DaniPopes <57450786+DaniPopes@users.noreply.github.com>
2023-01-06 16:17:38 +0100

    feat: Amount utils

--- src/common/mod.rs
+++ src/common/mod.rs
@@ -10,8 +10,22 @@ use ethers_core::types::U256;
 /// A helper enum that wraps a [U256] for determining a swap's input / output amount.
 #[derive(Clone, Copy, Debug, Eq, Hash, PartialEq, PartialOrd, Ord)]
 pub enum Amount {
-    /// Swap exactly {0} Token1 for any amount of Token2.
+    /// Swap exactly {0} `TokenIn` for any amount of `TokenOut`.
     ExactIn(U256),
-    /// Swap any amount of Token1 for exactly {0} Token2.
+    /// Swap any amount of `TokenIn` for exactly {0} `TokenOut`.
     ExactOut(U256),
+}
+
+impl Amount {
+    /// Swap exactly `amount` `TokenIn` for any amount of `TokenOut`.
+    #[inline]
+    pub fn exact_in<T: Into<U256>>(amount: T) -> Self {
+        Self::ExactIn(amount.into())
+    }
+
+    /// Swap any amount of `TokenIn` for exactly `amount` `TokenOut`.
+    #[inline]
+    pub fn exact_out<T: Into<U256>>(amount: T) -> Self {
+        Self::ExactOut(amount.into())
+    }
 }
